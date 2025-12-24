0651f736e668e341ce48894381033d6b8c3239e2
Victor Zverovich <viz@meta.com>
2025-12-23 18:33:04 -0800

    Add a verification program

--- test/verify.cc
+++ test/verify.cc
@@ -2,28 +2,40 @@
 // Copyright (c) 2025 - present, Victor Zverovich
 // Distributed under the MIT license (see LICENSE).
 
-#include "zmij.h"
-
 #include <math.h>    // isnan
-#include <stdio.h>   // sscanf
 #include <stdint.h>  // uint32_t
+#include <stdio.h>   // printf
 #include <string.h>  // memcpy
 
+#include <charconv>
+
+#include "zmij.h"
+
 int main() {
-  char buf[25] = {};
+  char actual[25] = {};
+  char expected[25] = {};
   unsigned long long i = 0, n = ~uint32_t();
   double percent = 100.0 / n;
   for (; i <= n; ++i) {
-    if (i % 10'000'000 == 0) printf("Progress: %.2f%%\n", i * percent);
+    if (i % 50'000'000 == 0) printf("Progress: %.2f%%\n", i * percent);
+
     uint32_t bits = i;
-    float expected = 0;
-    memcpy(&expected, &bits, sizeof(float));
-    zmij::to_string(expected, buf);
-    float actual = 0;
-    sscanf(buf, "%g", &actual);
-    if (actual == expected) continue;
-    if (isnan(actual) && isnan(expected)) continue;
-    printf("Roundtrip failure: %.9g != %.9g\n", actual, expected);
+    float f = 0;
+    memcpy(&f, &bits, sizeof(float));
+
+    zmij::to_string(f, actual);
+    *std::to_chars(expected, expected + 25, f, std::chars_format::scientific)
+         .ptr = '\0';
+
+    if (strcmp(actual, expected) == 0) continue;
+    if (strcmp(actual, "0") == 0 && strcmp(expected, "0e+00") == 0) continue;
+    if (strcmp(actual, "-0") == 0 && strcmp(expected, "-0e+00") == 0) continue;
+    if (strcmp(actual, "nan") == 0 && strncmp(expected, "nan", 3) == 0)
+      continue;
+    if (strcmp(actual, "-nan") == 0 && strncmp(expected, "-nan", 4) == 0)
+      continue;
+
+    printf("Roundtrip failure: %s != %s\n", actual, expected);
     return 1;
   }
   printf("Tested %lld values\n", i);
