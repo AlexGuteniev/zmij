cb2e52698baabceac10d6afca2b0a9951d3e0e63
Victor Zverovich <viz@meta.com>
2026-01-16 09:00:41 -0800

    Advance known good commit

--- test/benchmark-all-commits.py
+++ test/benchmark-all-commits.py
@@ -70,11 +70,11 @@ def main():
         run(["git", "clone", "https://github.com/vitaut/zmij.git",
              str(workdir)])
 
-        # efc82ab765b00adccc5f096d682b705f082113e5 is a known good commit
+        # 7a60b2667c52c328c574fbba0d08e75808e74d2a is a known good commit
         # with all improvements and regression before it accounted for.
         commits = run(
             ["git", "rev-list", "--reverse", "--topo-order",
-             "efc82ab765b00adccc5f096d682b705f082113e5..HEAD"],
+             "7a60b2667c52c328c574fbba0d08e75808e74d2a..HEAD"],
             cwd=workdir
         ).split()
 
